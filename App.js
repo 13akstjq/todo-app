c5b286cde97b4c45784ef9cef6f9ef9efde25847
13akstjq <13akstjq@naver.com>
2019-06-07 15:50:18 +0900

    Todo.js add commit

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import React , {Component}from 'react';
-import { StyleSheet, Text, View,StatusBar,Dimensions ,Platform } from 'react-native';
-
+import { StyleSheet, Text, View,StatusBar,Dimensions ,Platform ,TextInput,ScrollView} from 'react-native';
+import Todo from "./Todo.js";
 const {height,width} = Dimensions.get("window");
 export default class App extends Component {
+
+  state = {
+    newToDo : ""
+  };
+
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo : text
+    })
+  }
+
   render(){
+    const {newToDo} = this.state;
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content"></StatusBar>
+        <StatusBar barStyle="light-content" ></StatusBar>
         <Text style={styles.title}>To-do App</Text>
         <View style={styles.card}>
-          <Text>New to do</Text>
+          <TextInput style={styles.input}
+           placeholder={"new todo"}
+           value = {newToDo}
+           placeholderTextColor={"#999"}
+           onChangeText={this._controlNewToDo}
+           returnKeyType={"done"}
+           autoCorrect={false}
+           ></TextInput>
+           <ScrollView>
+             <Todo></Todo>
+           </ScrollView>
         </View>
       </View>
     );
@@ -24,8 +46,8 @@ const styles = StyleSheet.create({
   },
   title : {
     color : "white",
-    fontSize : 25,
-    marginTop : 20,
+    fontSize : 40,
+    marginTop : 35,
     fontWeight : "100",
     marginBottom : 30
   },
@@ -50,5 +72,12 @@ const styles = StyleSheet.create({
         elevation : 5
       }
     })
+  },
+  input : {
+    padding : 20,
+    fontSize : 42,
+    borderBottomColor : "#ddd",
+    borderBottomWidth : 1
   }
+
 });
