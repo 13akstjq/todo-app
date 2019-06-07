3a9af8d3bec4b5adc6ef0bd5ff6b17a8bc4ff31b
13akstjq <13akstjq@naver.com>
2019-06-07 23:02:42 +0900

    add todo finish commit

--- App.js
+++ App.js
@@ -2,34 +2,68 @@ import React , {Component}from 'react';
 import { StyleSheet, Text, View,StatusBar,Dimensions ,Platform ,TextInput,ScrollView} from 'react-native';
 import {AppLoading} from "expo";
 import Todo from "./Todo.js";
+import uuidv1 from "uuid/v1"
 const {height,width} = Dimensions.get("window");
 export default class App extends Component {
 
   state = {
     newToDo : "",
-    loadedToDos: true
+    loadedToDos: false,
+    toDos: {}
   };
 
   _controlNewToDo = text => {
     this.setState({
       newToDo : text
-    })
-  }
+    });
+  };
 
   _loadToDos = () => {
+    this.setState({
+      loadedToDos : true
+    });
+  };
 
-  }
-
+  _addTodo = () => {
+    const {newToDo} = this.state;
+    
+    this.setState(prevState =>{
+      const ID = uuidv1();
+      const newToDoObject ={
+        [ID]:{
+          id : ID,
+          isCompleted : false,
+          text : newToDo,
+          createdAt: Date.now(),
+        }
+      };
+      const newState = {
+        ...prevState,
+        newToDo: "",
+          toDos : {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        };
+        if(newToDo !== ""){ // 입력한 todo가 있다면 
+          this.setState({
+            newToDo : ""
+          });//마지막에 삭제하기 
+        }
+        return {...newState};
+    })
+  };
   componentDidMount(){
-
+  this._loadToDos();
   }
   
 
   render(){
-    const {newToDo,loadedToDos} = this.state;
-    // if(!loadedToDos){
-    //   return <AppLoading/>
-    // }
+    const {newToDo,loadedToDos,toDos} = this.state;
+    console.log(toDos);
+    if(!loadedToDos){
+      return <AppLoading/>
+    }
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" ></StatusBar>
@@ -42,9 +76,10 @@ export default class App extends Component {
            onChangeText={this._controlNewToDo}
            returnKeyType={"done"}
            autoCorrect={false}
+           onSubmitEditing={this._addTodo}
            ></TextInput>
            <ScrollView contentContainerStyle={styles.toDos}>
-             <Todo text={"Hello i'm To do"}></Todo>
+             {Object.values(toDos).map(toDo => <Todo key={toDo.id} {...toDo}></Todo>)}
            </ScrollView>
         </View>
       </View>
